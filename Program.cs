7c6b3d2ef64556c6259dae143d3290f79669f232
t-j <t-j@terence-john.co.uk>
2023-05-27 21:32:20 +0100

    overnight

--- Program.cs
+++ Program.cs
@@ -1,89 +1,78 @@
-﻿
-namespace L4751;
+﻿namespace L4751;
+
+using System;
+using System.Linq;
+
 class Program
 {
     static int[] triangle = { 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210, 231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496, 528, 561, 595, 630, 666, 703, 741, 780, 820, 861, 903, 946, 990 };
+
     static void Main(string[] args)
     {
+        var ac01 = new PropList();
+        var ac03 = new PropList();
+        var dn01 = new PropList();
+        var dn02 = new PropList();
+
         // -- dn02 : Palindrome and multiple of 5 with MP of 2
-        System.Console.WriteLine("dn02 : Palindrome and multiple of 5 with MP of 2");
-        for ( int i = 0 ; i < 10 ; i++)
+        // -- so we know that the first and last digits are both 5
+        Console.WriteLine("dn02 : Palindrome and multiple of 5 with MP of 2");
+        for (int num = 505; num < 600; num += 10)
         {
-            int num = 505 + 10 * i;
-            var values = GetValues(num);
-            if (values[3] == 2)
-            PrintValues(values);
-        }
-        System.Console.WriteLine();
-
-        // -- ac01 : ans is either 153 or 351
-        System.Console.WriteLine("ac01 : triangular with triangular DP");
-        foreach (var num in triangle)
-        {
-            if (num > 99 && (num / 10) % 10 == 5)
+            var props = new Properties(num);
+            if (props.MultiplicativePersistance == 2)
             {
-                var values = GetValues(num);
-                if (triangle.Contains(values[2]))
-                    PrintValues(values);
+                Console.WriteLine(props);
+                dn02.Add(props);
             }
-        }
-        System.Console.WriteLine();
-
-        // -- ac03 : Square number with a square DS 
-        // -- dn01 :
-        System.Console.WriteLine("dn01 : (DS + DP) is odd multiple of 5");
-        for (int a = 10; a < 40; a += 20)
-        {
-            for (int b = 1; b < 10; b++)
-            {
-                int num = a + b;
-                var values = GetValues(num);
-                if ((values[1] + values[2]) % 10 == 5)
-                    PrintValues(values);
-            }
-        }
-        System.Console.WriteLine();
-    }
-
-    static void PrintValues(int[] values)
-    {
-        foreach (var val in values)
-        {
-            System.Console.Write($"{val,5}");
         }
         Console.WriteLine();
 
-    }
-    static int[] GetValues(int num)
-    {
-        int[] retval = { num, 0, 1, 1 };
-        List<int> digits = GetDigits(num);
-        foreach (var digit in GetDigits(num))
+        // -- ac01 : triangular with triangular DP
+        Console.WriteLine("ac01 : triangular with triangular DP");
+        var dn02Digit0 = dn02.DigitsAtPosition(0);
+        foreach (var num in triangle.Where(x => x > 99))
         {
-            retval[1] += digit;
-            retval[2] *= digit;
-        }
-        int mp = retval[2];
-        while (mp > 9)
-        {
-            retval[3]++;
-            int product = 1;
-            foreach (var digit in GetDigits(mp))
+            var props = new Properties(num);
+            if (dn02Digit0.Contains(props.DigitAtPosition(1)) &&
+                triangle.Contains(props.DigitProduct))
             {
-                product *= digit;
+                Console.WriteLine(props);
+                ac01.Add(props);
             }
-            mp = product;
         }
-        return retval;
-    }
-    static List<int> GetDigits(int num)
-    {
-        var retval = new List<int>();
-        while (num > 0)
+        Console.WriteLine();
+
+        // -- dn01 : (DS + DP) is odd multiple of 5
+        Console.WriteLine("dn01 : (DS + DP) is odd multiple of 5");
+        var ac01Digit0 = ac01.DigitsAtPosition(0);
+        for (int num = 10; num < 100; num++)
         {
-            retval.Add(num % 10);
-            num /= 10;
+            var props = new Properties(num);
+            if ((props.DigitSum + props.DigitProduct) % 10 == 5 &&
+                ac01Digit0.Contains(props.DigitAtPosition(0)))
+            {
+                Console.WriteLine(props);
+                dn01.Add(props);
+            }
         }
-        return retval;
+        Console.WriteLine();
+
+        // -- ac03 : Square number with a square DS
+        Console.WriteLine("ac03 : Square number with a square DS");
+        for (int i = 10; i < 31; i++)
+        {
+            int num = i * i;
+            var props = new Properties(num);
+            if (props.DigitAtPosition(1) != 0 &&
+                props.DigitAtPosition(2) != 0 &&
+                new int[] { 1, 4, 9, 16, 25 }.Contains(props.DigitSum))
+            {
+                Console.WriteLine(props);
+                ac03.Add(props);
+            }
+        }
+        Console.WriteLine();
     }
+
 }
---
+++ Properties.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace L4751
+{
+    public class Properties
+    {
+        public int Number { get; set; }
+        public int[] Digits { get; set; }
+        public int DigitSum { get; set; }
+        public int DigitProduct { get; set; }
+        public int MultiplicativePersistance { get; set; }
+
+        public Properties(int num)
+        {
+            Number = num;
+
+            string numstr = Number.ToString();
+            Digits = new int[numstr.Length];
+            for ( int i = 0; i < numstr.Length; i++)
+            {
+                Digits[i] = int.Parse(numstr[i].ToString());
+            }
+
+            DigitSum = 0;
+            DigitProduct = 1;
+            foreach (var digit in GetDigits(Number))
+            {
+                DigitSum += digit;
+                DigitProduct *= digit;
+            }
+
+            int mp = DigitProduct;
+            MultiplicativePersistance = 1;
+            while (mp > 9)
+            {
+                MultiplicativePersistance++;
+                int product = 1;
+                foreach (var digit in GetDigits(mp))
+                {
+                    product *= digit;
+                }
+                mp = product;
+            }
+        }
+
+        public int DigitAtPosition(int pos)
+        {
+            return Digits[pos];
+        }
+
+        public override string ToString()
+        {
+            return $"{Number,4} {DigitSum,4} {DigitProduct,4} {MultiplicativePersistance,4}";
+        }
+
+        private List<int> GetDigits(int num)
+        {
+            var retval = new List<int>();
+            while (num > 0)
+            {
+                retval.Add(num % 10);
+                num /= 10;
+            }
+            return retval;
+        }
+    }
+}
+
